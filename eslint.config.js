266323a4a3d41fb4b634c054659a580e1643bdcf
jebarsanthatcros <jebrsanthatcroos@gmail.com>
2026-07-27 07:00:55 +0530

    Fix ESLint test globals

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -16,6 +17,20 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+  },
+
+
+  {
+    files: ['**/*.test.js', '**/*.test.jsx', '**/*.spec.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        describe: 'readonly',
+        it: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+      },
     },
   },
 ])
